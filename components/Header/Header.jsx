be430642f8bfdba2caea183fbd8ce006f3692d5f
mjlcreative <markjaysonlacandula@gmail.com>
2022-07-23 01:53:41 +0800

    Banner Update

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -7,11 +7,11 @@ const Header = () => {
     <div className="h-screen object-contain">
       <div className="relative h-full pt-60 pb-60">
         <img
-          className="absolute flex-auto w-full h-full left-0 top-0 right-0 bottom-0 max-w-none flex object-cover blur-sm"
-          src="/test.jpg/"
+          className="absolute flex-auto w-full h-full left-0 top-0 right-0 bottom-0 max-w-none flex object-cover"
+          src="/banner.jpg/"
           alt="bg-image"
         />
-        <div className="absolute bg-black opacity-20  left-0 top-0 right-0 bottom-0 w-full h-full mr-auto ml-auto" />
+        <div className="absolute bg-black opacity-30  left-0 top-0 right-0 bottom-0 w-full h-full mr-auto ml-auto" />
         <div className="container relative max-w-full mr-auto ml-auto px-12">
           <div className="grid justify-items-center text-center grid-cols-1 gap-y-2">
             <p className="text-2xl md:text-4xl lg:text-5xl xl:text-5xl text-white">
